0b04850a00e6ea59d3e044bcdf286fd8c46997e1
Mohammed shifin.k <99720958+Mohdshifin@users.noreply.github.com>
2024-07-09 16:41:50 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [blogs,setBlogs]=useState([
+        { title:'Brillent Times..', sub:'math', author:'Mario', id:1},
+        { title:'Rich Dad Poor Dad..', sub:'cs', author:'Asiue Usman', id:2},
+        { title:'Times of India', sub:'MCA', author:'Lukuman ali fukri', id:3}
+    ]);
     return (
         <div className="home" >
-            <h2>Home Page</h2>
+          {blogs.map((blog) =>(
+            <div className="blog-preview" key={blog.id}>
+                <h2>{ blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </div>
+          ))}
         </div>
       );
 }
