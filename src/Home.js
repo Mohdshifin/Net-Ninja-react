171af3a82e21d78a7110fbf6192caaab0b79618a
Mohammed shifin.k <99720958+Mohdshifin@users.noreply.github.com>
2024-07-10 16:26:30 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
@@ -7,14 +8,16 @@ const Home = () => {
         { title:'Rich Dad Poor Dad..', sub:'cs', author:'Asiue Usman', id:2},
         { title:'Times of India', sub:'MCA', author:'Lukuman ali fukri', id:3}
     ]);
+
+    const handleDelete = (id)=>{
+      const newBlog = blogs.filter( blog => blog.id !== id);
+      setBlogs(newBlog);
+    }
+
     return (
         <div className="home" >
-          {blogs.map((blog) =>(
-            <div className="blog-preview" key={blog.id}>
-                <h2>{ blog.title}</h2>
-                <p>Written by {blog.author}</p>
-            </div>
-          ))}
+          <h2>My Blogs..</h2>
+         <BlogList blog={blogs} handleDelete={handleDelete} />
         </div>
       );
 }
